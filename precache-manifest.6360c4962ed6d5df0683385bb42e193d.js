3f9cf76be9daaf7e5f098934096c8407358ace56
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2020-01-13 18:04:21 -0500

    Updates

---
+++ precache-manifest.6360c4962ed6d5df0683385bb42e193d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35ec654b68aa2dc00558717f721254f",
+    "url": "/blog-clone/index.html"
+  },
+  {
+    "revision": "1ec64430c0575c0029d8",
+    "url": "/blog-clone/static/js/2.4566ff88.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/blog-clone/static/js/2.4566ff88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8166842d5a6cbd300259",
+    "url": "/blog-clone/static/js/main.2c49a892.chunk.js"
+  },
+  {
+    "revision": "b77e178461b211ed56b4",
+    "url": "/blog-clone/static/js/runtime-main.a8a38dc7.js"
+  }
+]);
